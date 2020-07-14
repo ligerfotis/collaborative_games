0ce44fca9266d9afcfae65372a8679aabe621771
ligerfotis <ligerfotis@gmail.com>
2020-07-14 10:19:37 +0300

    plots

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot(time_elpsd, data, figure_title, y_axis_name, x_axis_name, path, save=True):
+def plot(time_elpsd, data, figure_title, y_axis_name, x_axis_name, path, save=True, variance=False, stdev=None):
     plt.figure(figure_title)
     plt.grid()
     # plt.xticks(np.arange(0, time_elpsd[-1], step=500))
@@ -10,6 +10,8 @@ def plot(time_elpsd, data, figure_title, y_axis_name, x_axis_name, path, save=Tr
     plt.plot(time_elpsd, data)
     plt.ylabel(y_axis_name)
     plt.xlabel(x_axis_name)
+    if variance:
+        plt.fill_between(time_elpsd, np.array(data) - np.array(stdev), np.array(data) + np.array(stdev))
     if save:
         plt.savefig(path + figure_title)
         plt.show()
